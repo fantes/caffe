352ab73d0ea038741ea2d92ae621aba0f35fc9f1
Guillaume Infantes <guillaume.infantes@jolibrain.com>
2021-02-25 15:38:17 +0100

    try to fix refinedet

--- src/caffe/layers/detection_output_layer.cu
+++ src/caffe/layers/detection_output_layer.cu
@@ -74,12 +74,12 @@ void DetectionOutputLayer<Dtype>::Forward_gpu(
          {
            OSPermuteDataGPU<Dtype>(bottom[1]->count(),
                                    bottom[bottom.size()-1]->gpu_data(), bottom[3]->gpu_data(),
-                                   num_classes_, num_priors_, 1, logit_permute_data,objectness_score_);
+                                   num_classes_, num_priors_, num, logit_permute_data,objectness_score_);
          }
        else
          {
           PermuteDataGPU<Dtype>(bottom[1]->count(), bottom[bottom.size()-1]->gpu_data(),
-                                num_classes_, num_priors_, 1, logit_permute_data);
+                                num_classes_, num_priors_, num, logit_permute_data);
          }
        logit_cpu_data = logit_permute_.cpu_data();
     }
